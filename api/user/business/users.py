53fb7dc468322a2a27271bd6f4b5db1d15e812a9
Francisco Palacios A <34322522+FranciscoPalaciosA@users.noreply.github.com>
2021-03-16 19:00:54 -0600

    Endpoint for creating qr code

--- api/user/business/users.py
+++ api/user/business/users.py
@@ -11,14 +11,12 @@ def create_user(args):
     secret = generate_secret_key(seed)
     user = {
         'email': args["email"], 
-        'name': args["name"],
+        'name': args["fullName"],
         'secret': secret
     }
-    
     u_id = uuid.uuid4()
     ref = get_reference(f'users/{u_id}')
     ref.set(user)
-
     return f'email:{args["email"]}-secret:{secret}'
 
 def does_user_exist(email_to_check):
